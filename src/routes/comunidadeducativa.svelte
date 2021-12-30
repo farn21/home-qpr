14019db601b534b8df602a9baf70ef2c8cb70631
FARN (Fundación Ambiente y Recursos Naturales) <qpr@farn.org.ar>
2021-12-30 08:57:05 -0300

    Fixes navigation (#1)
    * Multipage application
    * Text changes
    
    Co-authored-by: Pablo Martin Perez <pablomartin.it@gmail.com>
    Co-authored-by: malenavelarde <malevelarde@gmail.com>

---
+++ src/routes/comunidadeducativa.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Button from '$lib/jui-components/Button.svelte';
+  import { goto } from '$app/navigation';
+</script>
+
+<svelte:head>
+		<title>Ingreso por primera vez</title>
+</svelte:head>
+
+<div class='container'>
+
+		<div class="gradient-background">
+			  <div class='user-type-card education-card'>
+				    <div class='logo-container'>
+						    <img class='card-logo' src='/images/Logo QPR_1.png' alt='logo QPR' />
+				    </div>
+				    <div class='card-header'>
+					      <div class='card-header-title'>
+						        <div class='icon-container'>
+							          <img class='card-icon' src='/icons/Asset_education.png' alt='ícono comunidad educativa' />
+						        </div>
+						        <h1 class='c-h2'>Comunidad educativa</h1>
+					      </div>
+				    </div>
+				    <p class='bold-text'>Querés concientizar sobre la importancia de cuidar el medio ambiente y llevar adelante acciones pedagógicas para valorar los recursos bio-culturales de la cuenca.</p>
+				    <div class='card-body'>
+					      <p class='regular-text'>· Podés encontrar información pública sobre procesos de protección de áreas naturales, calidad de agua y procesos de relocalización de los barrios, villas y asentamientos producida por ACUMAR, RENABAP, ADA y SINIA.</p>
+					      <p class='regular-text'>· Podés poner en práctica algunos principios de la Ley de Educación Ambiental como la participación y formación ciudadana y el cuidado del patrimonio natural y cultural.</p>
+					      <p class='regular-text'>· Podés organizar salidas didácticas y contribuir a generar datos sobre estos temas.</p>
+					  </div>
+			  </div>
+			  <div class="botonera">
+				    <Button
+				        class='mobile-button'
+					      skin="secondary-white"
+					      href='https://quepasa.mapseed.org'
+				    >
+					      Ingreso al mapa
+				    </Button>
+			  </div>
+		</div>
+
+</div>
---
+++ src/routes/primeravez.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Button from '$lib/jui-components/Button.svelte';
+  import { goto } from '$app/navigation';
+</script>
+
+<svelte:head>
+    <title>Ingreso por primera vez</title>
+</svelte:head>
+
+<div class='container'>
+
+	  <div class="gradient-background">
+		    <div class='ingreso'>
+			      <div class='logo-container'>
+				        <img class='card-logo' src='/images/Logo QPR_1.png' alt='logo QPR' />
+			      </div>
+			      <p class='title'>Ingreso como</p>
+			      <div class="botonera card-button-container">
+				        <Button
+					          skin="tertiary"
+					          on:click={()=>{
+                             goto('/vecinocuenca')
+					          }}
+				        >
+					          Habitante u organización de la Cuenca
+				        </Button>
+				        <Button
+					          skin="tertiary"
+					          on:click={()=>{
+                             goto('/comunidadeducativa')
+					          }}
+				        >
+					          Comunidad educativa
+				        </Button>
+				        <Button
+					          skin="tertiary"
+					          on:click={()=>{
+                             goto('/comunidadcientifica')
+					          }}
+				        >
+					          Comunidad científica
+				        </Button>
+				        <Button
+					          skin="tertiary"
+					          on:click={()=>{
+                             goto('/gobierno')
+					          }}
+				        >
+					          Gobierno
+				        </Button>
+			      </div>
+		    </div>
+		</div>
+
+</div>
